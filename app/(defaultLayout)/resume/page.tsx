0de0bda70666a277ad550d219ccfb54b81f2b049
Felipe Mullen <felipedmullen@gmail.com>
2024-02-23 16:13:45 -0800

    update resume

--- app/(defaultLayout)/resume/page.tsx
+++ app/(defaultLayout)/resume/page.tsx
@@ -4,7 +4,7 @@ export default function ResumePage() {
             <div className="h-full w-full flex flex-col">
                 <div className="flex justify-between items-center mt-2 bg-zinc-50/50 p-3 rounded border">
                     <span className="text-neutral-800">Felipe Mullen, <i>curriculum vitae</i></span>
-                    <small className="text-neutral-600">(last updated: January 2024)</small>
+                    <small className="text-neutral-600">(last updated: February 2024)</small>
                 </div>
                 <div className="grow w-full p-3 my-3 bg-zinc-50 border rounded">
                     <iframe className="w-full h-1/2" src="/resume/felipe_mullen_cv_light_2024.pdf" />
